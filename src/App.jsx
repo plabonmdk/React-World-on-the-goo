724082fa1a3cbcf41326bc488bd2aab64b9a2441
plabonmdk <plabonmdk@gmail.com>
2025-09-18 21:39:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Country from './componants/Country'
+
+function App() {
+ 
+
+  return (
+    <>
+ 
+      <h1>React World on tha go ..........</h1>
+      <Country></Country>
+      
+    </>
+  )
+}
+
+export default App
